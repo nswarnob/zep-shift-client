320bc79bca54fce358d78607fec9fe7a99153c5d
nswarnob <nswarnob@gmail.com>
2025-11-24 23:48:45 +0600

    Dynamic payment updated with user confirmation!

--- src/Pages/sendParcel/SendParcel.jsx
+++ src/Pages/sendParcel/SendParcel.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useForm, useWatch } from "react-hook-form";
 import { useLoaderData } from "react-router";
+import Swal from "sweetalert2";
 
 export const SendParcel = () => {
   const { register, control, handleSubmit } = useForm({ mode: "onSubmit" });
@@ -26,6 +27,49 @@ export const SendParcel = () => {
 
   const handleSendParcel = (data) => {
     console.log(data);
+    
+    const isDocument = data.parcelType === 'document';
+    const isSameDistrict = data.senderDistrict === data.receiverDistrict;
+  const parcelWeight = parseFloat(data.parcelWeight);
+
+  let cost = 0;
+
+  if(isDocument){
+    cost = isSameDistrict? 60:80 ;
+  }
+  else{
+    if(parcelWeight < 3){
+      cost = isSameDistrict? 110 : 150;
+    } else{
+      const minCharge = isSameDistrict?110:150;
+      const extraWeight = parcelWeight - 3;
+      const extraCharge = isSameDistrict? extraWeight*40: extraWeight*40 + 40;
+
+      cost = minCharge + extraCharge;
+    }
+  }
+
+  Swal.fire({
+  title: "Agree with this cost?",
+  text: `You will be charged ${cost} taka.`,
+  icon: "warning",
+  showCancelButton: true,
+  confirmButtonColor: "#3085d6",
+  cancelButtonColor: "#d33",
+  confirmButtonText: "Yes, take it!"
+}).then((result) => {
+  if (result.isConfirmed) {
+    Swal.fire({
+      title: "Deleted!",
+      text: "Your file has been deleted.",
+      icon: "success"
+    });
+  }
+});
+
+  
+  
+  
   };
 
   return (
